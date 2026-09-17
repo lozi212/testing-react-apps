5d3803f855b0b6480b171d95d4421b450a6fcb15
Loza Demeke <your-github-email@example.com>
2026-09-17 23:42:37 +0300

    Complete exercise 04

--- src/__tests__/exercise/04.js
+++ src/__tests__/exercise/04.js
@@ -4,25 +4,45 @@
 import * as React from 'react'
 import {render, screen} from '@testing-library/react'
 import userEvent from '@testing-library/user-event'
+import {build, fake} from '@jackfranklin/test-data-bot'
 import Login from '../../components/login'
 
-test('submitting the form calls onSubmit with username and password', () => {
-  // 🐨 create a variable called "submittedData" and a handleSubmit function that
-  // accepts the data and assigns submittedData to the data that was submitted
-  // 💰 if you need a hand, here's what the handleSubmit function should do:
-  // const handleSubmit = data => (submittedData = data)
-  //
-  // 🐨 render the login with your handleSubmit function as the onSubmit prop
-  //
-  // 🐨 get the username and password fields via `getByLabelText`
-  // 🐨 use `await userEvent.type...` to change the username and password fields to
-  //    whatever you want
-  //
-  // 🐨 click on the button with the text "Submit"
-  //
-  // assert that submittedData is correct
-  // 💰 use `toEqual` from Jest: 📜 https://jestjs.io/docs/en/expect#toequalvalue
+const buildLoginForm = build({
+  fields: {
+    username: fake(f => f.internet.userName()),
+    password: fake(f => f.internet.password()),
+  },
 })
+
+test(
+  'submitting the form calls onSubmit with username and password',
+  async () => {
+    const handleSubmit = jest.fn()
+
+    render(<Login onSubmit={handleSubmit} />)
+
+    const {username, password} = buildLoginForm()
+
+    await userEvent.type(
+      screen.getByLabelText(/username/i),
+      username,
+    )
+
+    await userEvent.type(
+      screen.getByLabelText(/password/i),
+      password,
+    )
+
+    await userEvent.click(
+      screen.getByRole('button', {name: /submit/i}),
+    )
+
+    expect(handleSubmit).toHaveBeenCalledWith({
+      username,
+      password,
+    })
+  },
+)
 
 /*
 eslint
