43d19e801f5a7497d2e6c66f1c8f47e62d44ff05
Loza Demeke <your-github-email@example.com>
2026-09-17 20:14:23 +0300

    Complete exercise 01

--- src/__tests__/exercise/01.js
+++ src/__tests__/exercise/01.js
@@ -2,34 +2,53 @@
 // http://localhost:3000/counter
 
 import * as React from 'react'
-import {act} from 'react-dom/test-utils'
-import {createRoot} from 'react-dom/client'
+import ReactDOM from 'react-dom'
 import Counter from '../../components/counter'
 
-// NOTE: this is a new requirement in React 18
-// https://react.dev/blog/2022/03/08/react-18-upgrade-guide#configuring-your-testing-environment
-// Luckily, it's handled for you by React Testing Library :)
-global.IS_REACT_ACT_ENVIRONMENT = true
-
 test('counter increments and decrements when the buttons are clicked', () => {
-  // 🐨 create a div to render your component to (💰 document.createElement)
-  //
-  // 🐨 append the div to document.body (💰 document.body.append)
-  //
-  // 🐨 use createRoot to render the <Counter /> to the div
-  // 🐨 get a reference to the increment and decrement buttons:
-  //   💰 div.querySelectorAll('button')
-  // 🐨 get a reference to the message div:
-  //   💰 div.firstChild.querySelector('div')
-  //
-  // 🐨 expect the message.textContent toBe 'Current count: 0'
-  // 🐨 click the increment button (💰 act(() => increment.click()))
-  // 🐨 assert the message.textContent
-  // 🐨 click the decrement button (💰 act(() => decrement.click()))
-  // 🐨 assert the message.textContent
-  //
-  // 🐨 cleanup by removing the div from the page (💰 div.remove())
-  // 🦉 If you don't cleanup, then it could impact other tests and/or cause a memory leak
+  // create a div to render your component to
+  const div = document.createElement('div')
+
+  // append the div to document.body
+  document.body.append(div)
+
+  // use ReactDOM.render to render the <Counter /> to the div
+  ReactDOM.render(<Counter />, div)
+
+  // get a reference to the increment and decrement buttons
+  const [decrement, increment] = div.querySelectorAll('button')
+
+  // get a reference to the message div
+  const message = div.firstChild.querySelector('div')
+
+  // expect the message.textContent toBe 'Current count: 0'
+  expect(message.textContent).toBe('Current count: 0')
+
+  // click the increment button
+  increment.click()
+
+  // assert the message.textContent
+  expect(message.textContent).toBe('Current count: 1')
+
+  // click the decrement button
+  decrement.click()
+
+  // assert the message.textContent
+  expect(message.textContent).toBe('Current count: 0')
+
+  // Extra Credit: use dispatchEvent
+  increment.dispatchEvent(
+    new MouseEvent('click', {
+      bubbles: true,
+      cancelable: true,
+      button: 0,
+    }),
+  )
+
+  expect(message.textContent).toBe('Current count: 1')
+
+  // cleanup by removing the div from the page
+  div.remove()
 })
 
 /* eslint no-unused-vars:0 */
